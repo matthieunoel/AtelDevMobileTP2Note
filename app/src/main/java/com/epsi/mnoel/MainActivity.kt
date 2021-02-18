295747ed548fc783b7303738e00b8ca0612fc1a0
MattUnicornBlue <matthieu.noel@epsi.fr>
2021-02-18 22:12:24 +0100

    Y A RIBERY!

--- app/src/main/java/com/epsi/mnoel/MainActivity.kt
+++ app/src/main/java/com/epsi/mnoel/MainActivity.kt
@@ -1,21 +1,34 @@
 package com.epsi.mnoel
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.Registry
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+import com.firebase.ui.storage.images.FirebaseImageLoader
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.ktx.initialize
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.InputStream
+import com.epsi.mnoel.FireBaseCompatibleGlideModule
+
 //import java.lang.reflect.Array
 
 
@@ -38,15 +51,11 @@ class MainActivity : AppCompatActivity() {
 
         val database: FirebaseDatabase = FirebaseDatabase.getInstance()
 
-        val databaseReference = database.reference
-        Log.i("databaseReference", databaseReference.toString())
-
-        val livres = database.getReference("livre")
-        Log.i("livres", livres.toString())
+        val dbLivresRef = database.getReference("livre")
 
         this.listeLivres = arrayOf()
 
-        livres.addValueEventListener(object : ValueEventListener {
+        dbLivresRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 for (data in dataSnapshot.children) {
 
@@ -66,7 +75,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
-                Log.i("MNOELTRUCUTRUC", "onCancelled: Error: " + databaseError.message)
+                Log.i("MNOELREADTHIS", "onCancelled: Error: " + databaseError.message)
             }
 
             override fun toString(): String {
@@ -74,13 +83,33 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
-        livres.get()
+        dbLivresRef.get()
 
         var manager = LinearLayoutManager(this)
-        var adpater = AdapterDuTurfu(arrayOf(Livre(0, "Chargement en cours ...", "Chargement en cours ...")))
+        var adpater = AdapterDuTurfu(
+            arrayOf(
+                Livre(
+                    0,
+                    "Chargement en cours ...",
+                    "Chargement en cours ..."
+                )
+            )
+        )
 
         this.recycler_view.adapter = adpater
         this.recycler_view.layoutManager = manager
+
+        /* ##### */
+
+//        var storage:FirebaseStorage  = FirebaseStorage.getInstance()
+//
+//        var livre1ImgRef = storage.getReference()
+//
+//        var img = livre1ImgRef.child("livre_1.png")
+//
+//        Log.i("MNOELREADTHIS", "Livre1Img : " + img)
+
+        /* ##### */
 
     }
 }
@@ -91,7 +120,72 @@ class AdapterDuTurfu(private val myDataSet: Array<Livre>):
     class ViewHolder(val view: View): RecyclerView.ViewHolder(view){
         fun bind(livre: Livre){
             val tv = view.findViewById<TextView>(R.id.titleView)
-            tv.text = livre.titre
+
+            if (livre.auteur != null) {
+                tv.text = livre.titre + "\r\n" + livre.auteur
+            }
+            else {
+                tv.text = livre.titre
+            }
+
+            /*if (livre.img != null) {
+//                view.findViewById<ImageView>(R.id.imageView).setImageURI(Uri.parse(livre.img))
+
+                val imageBytes = Base64.decode(livre.img, Base64.DEFAULT)
+                Log.i("MNOELREADTHIS", "imageBytes : $imageBytes")
+                val decodedImage = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+                Log.i("MNOELREADTHIS", "decodedImage : $decodedImage")
+//                view.findViewById<ImageView>(R.id.imageView).setImageBitmap(decodedImage)
+                view.findViewById<ImageView>(R.id.imageView).setImageBitmap(decodedImage)
+            }*/
+
+            /* ##### */
+
+            /*var storage:FirebaseStorage  = FirebaseStorage.getInstance()
+
+            var livre1ImgRef = storage.getReference()
+
+            var img = livre1ImgRef.child("images/livre_1.jpg")
+
+            Log.i("MNOELREADTHIS", "Livre1Img : " + img)*/
+
+            /* ##### */
+
+            /*// Reference to an image file in Cloud Storage
+//            StorageReference storageReference = = FirebaseStorage.getInstance().getReference().child("yourImageReferencePath");
+
+            var storageReference:Any  = FirebaseStorage.getInstance().getReference().child("images/livre_1.jpg");
+            Log.i("MNOELREADTHIS", "storageReference : $storageReference")
+
+//            ImageView image = (ImageView)findViewById(R.id.imageView);
+
+            var image: ImageView = view.findViewById<ImageView>(R.id.imageView)
+            Log.i("MNOELREADTHIS", "image : $image")
+
+            // Load the image using Glide
+            Glide.with(this.view)
+//                .using(FirebaseImageLoader())
+                .load(storageReference)
+                .into(image)*/
+
+            /* ##### */
+
+            // WORKING CODE!
+            val storage = FirebaseStorage.getInstance()
+
+            // Create a reference to a file from a Google Cloud Storage URI
+            val gsReference = storage.getReferenceFromUrl("gs://ateldevmobile-tp2.appspot.com/images/livre_1.jpg")
+
+            var imageView: ImageView = view.findViewById<ImageView>(R.id.imageView)
+
+            /*GlideApp.with(this.view)
+                .load(gsReference)
+                .into(imageView)*/
+
+            Glide.with(this.view)
+                .load(gsReference)
+                .into(imageView)
+
         }
     }
 
@@ -114,6 +208,8 @@ class Livre {
     var id: Int? = null
     var titre: String? = null
     var desc: String? = null
+    var auteur: String? = null
+    var img: String? = null
 
     constructor() {}
     constructor(
@@ -124,5 +220,18 @@ class Livre {
         this.id = id
         this.titre = titre
         this.desc = desc
+    }
+    constructor(
+        id: Int?,
+        titre: String?,
+        desc: String?,
+        auteur: String?,
+        img: String?
+    ) {
+        this.id = id
+        this.titre = titre
+        this.desc = desc
+        this.auteur = auteur
+        this.img = img
     }
 }
