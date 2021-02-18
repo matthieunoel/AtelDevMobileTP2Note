409cfdaf0d3ad360dc8fef3c64c116f612ef0f25
MattUnicornBlue <matthieu.noel@epsi.fr>
2021-02-18 20:02:27 +0100

    Login et recup de donnée fonctionnelle ^^'

--- app/src/main/java/com/epsi/mnoel/LoginActivity.kt
+++ app/src/main/java/com/epsi/mnoel/LoginActivity.kt
@@ -3,36 +3,75 @@ package com.epsi.mnoel
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.ktx.initialize
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_main.*
 
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+
 class LoginActivity : AppCompatActivity() {
+
+    private var emailContent = ""
+    private var passwordContent = ""
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         Firebase.initialize(application)
-
-
-
+        auth = Firebase.auth
 
         val submitBtn = findViewById<Button>(R.id.btn_submit)
 
         submitBtn.setOnClickListener {
-//            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
-            /*Intent intent = Intent(this, MainActivity.class);
-            startActivity(intent);*/
-
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-
+            clickConnection()
         }
 
+    }
+
+    private fun clickConnection() {
+        var formUsername = (findViewById<EditText>(R.id.form_username)).text.toString()
+        var formPassword = (findViewById<EditText>(R.id.form_password)).text.toString()
+
+        if (formUsername != "") {
+
+            if (formPassword == "") {
+                formPassword = " "
+            }
+
+            auth.signInWithEmailAndPassword(formUsername, formPassword).addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(
+                            baseContext, "Connexion reussie.",
+                            Toast.LENGTH_SHORT
+                    ).show()
+
+                    val user = auth.currentUser
+
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+
+                } else {
+                    this.alert("Erreur de connection", "Le couple Username/Password est incorrect.")
+                }
+            }
+
+        }
+        else {
+            this.alert("Nom d'utilisateur", "Erreur : ce champ ne peut pas être vide.")
+        }
+    }
+
+    private fun alert(alertTitle: String, alertMessage: String) {
+        AlertDialog.Builder(this).setTitle(alertTitle).setMessage(alertMessage).show()
     }
 
 }
--- app/src/main/java/com/epsi/mnoel/MainActivity.kt
+++ app/src/main/java/com/epsi/mnoel/MainActivity.kt
@@ -1,66 +1,102 @@
 package com.epsi.mnoel
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.ktx.initialize
 import kotlinx.android.synthetic.main.activity_main.*
+//import java.lang.reflect.Array
+
 
 //  https://console.firebase.google.com/u/0/project/ateldevmobile-tp2/settings/general/android:com.epsi.mnoel
 //  https://www.codevscolor.com/android-kotlin-create-basic-recyclerview
 
 class MainActivity : AppCompatActivity() {
 
-    val values = arrayOf("01", "Deux (plus du texte de test. Tu sais comment c'est lol ^^ . . . Anyway va te faire foutre ? Ouuaaaaaais mdr)" , "Trois", "Quatre", "05" , "Six", "07", "08", "Neuf", "10", "11", "Douze", "13", "14", "Quinze", "16", "Dix-sept", "18", "19", "Vingt", "21", "Vingt deux", "23", "24", "25", "26", "Vingt sept", "28", "29", "Trente")
-
     private lateinit var recyclerView: RecyclerView
     private lateinit var manager: RecyclerView.LayoutManager
     private lateinit var myAdapter: RecyclerView.Adapter<*>
+
+    private lateinit var listeLivres: Array<Livre>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /*RecyclerView rv = (RecyclerView) findViewById(R.id.list);
-        rv.setLayoutManager(new LinearLayoutManager(this))
-        rv.setAdapter(new MyAdapter());*/
-
         Firebase.initialize(application)
 
+        val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+
+        val databaseReference = database.reference
+        Log.i("databaseReference", databaseReference.toString())
+
+        val livres = database.getReference("livre")
+        Log.i("livres", livres.toString())
+
+        this.listeLivres = arrayOf()
+
+        livres.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (data in dataSnapshot.children) {
+
+                    val livre: Livre? = data.getValue(Livre::class.java)
+
+                    if (livre != null) {
+                        this@MainActivity.listeLivres += livre
+                    }
+                }
+
+                var manager = LinearLayoutManager(this@MainActivity)
+                var adpater = AdapterDuTurfu(this@MainActivity.listeLivres)
+
+                this@MainActivity.recycler_view.adapter = adpater
+                this@MainActivity.recycler_view.layoutManager = manager
+
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.i("MNOELTRUCUTRUC", "onCancelled: Error: " + databaseError.message)
+            }
+
+            override fun toString(): String {
+                return "FUCK YOU !"
+            }
+        })
+
+        livres.get()
+
         var manager = LinearLayoutManager(this)
-        var adpater = AdapterDuTurfu(values)
+        var adpater = AdapterDuTurfu(arrayOf(Livre(0, "Chargement en cours ...", "Chargement en cours ...")))
 
-        recycler_view.adapter = adpater
-        recycler_view.layoutManager = manager
-
-        /*recycler_view = findViewById<RecyclerView>(R.id.recycler_view).apply {
-            layoutManager = manager
-            adapter =  adpater
-        }*/
-
-//        list_test.layoutManager = LinearLayoutManager(this)
+        this.recycler_view.adapter = adpater
+        this.recycler_view.layoutManager = manager
 
     }
 }
 
-class AdapterDuTurfu(private val myDataSet : Array<String>):
+class AdapterDuTurfu(private val myDataSet: Array<Livre>):
     RecyclerView.Adapter<AdapterDuTurfu.ViewHolder>() {
 
     class ViewHolder(val view: View): RecyclerView.ViewHolder(view){
-        fun bind(text: String){
+        fun bind(livre: Livre){
             val tv = view.findViewById<TextView>(R.id.titleView)
-            tv.text = text
+            tv.text = livre.titre
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val vh = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent,false)
+        val vh = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
         return ViewHolder(vh)
     }
 
@@ -74,4 +110,19 @@ class AdapterDuTurfu(private val myDataSet : Array<String>):
 
 }
 
-data class Item(val id: Long, val title: String, val url: String)
+class Livre {
+    var id: Int? = null
+    var titre: String? = null
+    var desc: String? = null
+
+    constructor() {}
+    constructor(
+        id: Int?,
+        titre: String?,
+        desc: String?
+    ) {
+        this.id = id
+        this.titre = titre
+        this.desc = desc
+    }
+}
